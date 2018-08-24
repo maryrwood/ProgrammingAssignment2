e03e7b0791059069ed0cc5394164f3a2f5df7d99
maryrwood <39995495+maryrwood@users.noreply.github.com>
2018-08-24 17:41:39 -0400

    MaryWood
    This is my version of this assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,17 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This makes the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  set <- function(y){
+    x <<- y 
+    m <<- NULL
+  }
+  get <- function() x
+  mvar <<- m
+  get2 <- function() m
+  list(set = set, get = get, mvar = mvar, get2 = get2)
 }
 
 
@@ -12,4 +19,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$get2()
+  if(!is.null(m)){
+    message("Data was cached")
+    return(m)
+  }
+  
+  elseif(is.null(m)){
+    data <- x$get()
+    solve(x)
+    x$mvar(m)
+    m
+  }
 }
